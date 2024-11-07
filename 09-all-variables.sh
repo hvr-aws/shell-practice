78e866cdae86f58d98308a1a346d088965229509
Harsha <harsha.pakanathi@gmail.com>
2024-11-07 12:34:58 +0530

    shell script

--- 09-all-variables.sh
+++ 09-all-variables.sh
@@ -2,3 +2,9 @@
 
 echo "all variables passed in the script: $@"
 echo "number of variables passed is : $#"
+echo "current script name: $0"
+echo "current working directory : $PWD"
+echo " home directory of current user: $HOME"
+echo "PID of script executing now : $$"
+sleep 100 &
+echo "PID of last backgrounfd command: $!"
