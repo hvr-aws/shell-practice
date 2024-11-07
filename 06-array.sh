f414349acc60c9ae44bf9eb26e9ea381fa0443fe
Harsha <harsha.pakanathi@gmail.com>
2024-11-07 11:30:42 +0530

    shell script

--- 06-array.sh
+++ 06-array.sh
@@ -2,6 +2,6 @@
 
 FRUITS=("apple" "kiwi" "carrot")
 
-echo "first fruit is [0]"
-echo "second fruit is [1]"
-echo "all fruits are [@]"
+echo "first fruit is $FRUITS[0]"
+echo "second fruit is $FRUITS[1]"
+echo "all fruits are $FRUITS[@]"
