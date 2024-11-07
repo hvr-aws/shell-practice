400f258842730ff1c3b04e7ab63127e1ee54ca0a
Harsha <harsha.pakanathi@gmail.com>
2024-11-07 13:14:45 +0530

    shell script

--- 10.condition.sh
+++ 10.condition.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-NUMBER= $1
+NUMBER=$1
 
-if [ $NUMBER -gt 5 ]
+if [ $NUMBER -gt 20 ] #gt, lt, eq, -ne, -ge, -le
 then
-    echo "given number $NUMBER is grater than 5"
-
+    echo "Given number: $NUMBER is greater than 20"
 else
-
-    echo "given number $NUMBER is less than 5"
-
+    echo "Given number: $NUMBER is less than 20"
 fi
