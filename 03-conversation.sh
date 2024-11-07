9ecfa086842cf23f85732adcec3a52fbe78160bd
Harsha <harsha.pakanathi@gmail.com>
2024-11-07 11:07:56 +0530

    adding shell

---
+++ 03-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+M1=buddy
+M2=shuushuu
+
+echo "$M1 :: hi $M2 how are you"
+echo "$M2 :: Hi $M1 i am fine wht about you"
+echo "$M1:: $M2 where are you from"
+echo "$M2:: $M1 i am from hyd"
